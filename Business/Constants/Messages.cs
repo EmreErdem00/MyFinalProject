93359433181460c32a809d55dbb4659ac1e33774
EmreErdem <emre.erdm00@gmail.com>
2021-02-25 00:10:55 +0300

    12. Gün ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,6 +10,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string ProductsListed = "Ürün listelendi";
         public static string MaintenanceTime = "Bakım zamanı";
-
+        public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExist="Bu isimde zaten bir ürün var";
+        public static string CategoryLimitExceded ="KAtegori limiti aşılığı için ürün eklenmedi";
     }
 }
